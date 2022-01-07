432b59299e6029f8b1ec43a32e1e3bf12fba7fa5
nilzz <7029974@qq.com>
2022-01-08 02:04:12 +0800

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Filc
+//
+//  Created by box on 2021/11/5.
+//  Copyright © 2021 filc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
